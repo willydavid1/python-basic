a071cf353b2a618221c81da2762dee080652a3be
willy <willydavidlozada@gmail.com>
2021-09-26 16:18:37 -0500

    Password generator

---
+++ password_generator.py
@@ -0,0 +1,22 @@
+import random
+
+def generate_password():
+    capital_letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'X', 'Y', 'Z']
+    lowercase_letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'x', 'y', 'z']
+    numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+    characters = ['*', '+', '-', '/', '@', '_', '?', '!', '[', '{', '(', ')', '}', ']', ',', ';', '.', '>', '<', '~', '°', '^', '&', '$', '#', '"']
+
+    all_characters = [*capital_letters, *lowercase_letters, *numbers, *characters]
+    password = []
+
+    for i in range(50):
+        password.append(random.choice(all_characters))
+
+    return ''.join(password)
+
+def run():
+    password = generate_password()
+    print(f'Password: {password}')
+
+if __name__ == '__main__':
+    run()
